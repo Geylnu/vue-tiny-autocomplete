667d81f16135755e866da203b145798232c82838
yanglei1 <yanglei1@tuhu.cn>
2021-05-27 22:27:27 +0800

    feat: 添加安装方式

--- src/export.js
+++ src/export.js
@@ -1,7 +1,13 @@
 import BaseAutoComplete from "./components/BaseAutoComplete";
 import BaseInput from "./components/BaseInput";
 
+function install(Vue) {
+  Vue.component(BaseAutoComplete.name, BaseAutoComplete);
+  Vue.component(BaseInput.name, BaseInput);
+}
+
 export default {
+  install,
   BaseAutoComplete,
   BaseInput,
 };
